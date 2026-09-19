d247ddd129356f2fd3299ae19e029a8fc801adc3
reksadsp <accounts@reksa.fr>
2026-09-20 01:13:35 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Development environment with the Neocities CLI";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [pkgs.ruby_3_4 
+                        pkgs.openssl_3_5];
+            NIX_NO_SELF_RPATH = true;
+            shellHook = ''
+              sudo gem install neocities
+              bundle init
+              echo "gem 'neocities'" >> Gemfile
+              bundle install
+              bundle exec neocities login
+            '';
+          };
+        });
+    };
+}
